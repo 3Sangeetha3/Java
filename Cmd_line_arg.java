2a03d585483e94654463a6a292b531499d0e7303
sangeetha <jadamalsangeetha@gmail.com>
2024-01-24 22:16:48 +0530

    command line argument

---
+++ Cmd_line_arg.java
@@ -0,0 +1,13 @@
+///Command line argument 
+/// ->It is passes at the time of running the java program.
+/// ->In java, the command lne arguments passed from the console can be rececived in the java program and they can be used as input.
+
+public class Cmd_line_arg {
+    public static void main(String args[])
+    {
+        System.out.println(args[0]);            
+    }
+}
+
+//In terminal enter javac Cmd_line_arg.java and enter
+//tehn : java ex1 hello -> this command line argument.
