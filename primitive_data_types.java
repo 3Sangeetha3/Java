68da33ce2df5df0116f40cb825d9b1d96b733572
sangeetha <jadamalsangeetha@gmail.com>
2024-03-18 21:40:15 +0530

    primitive data types

---
+++ primitive_data_types.java
@@ -0,0 +1,33 @@
+//Primitive datatypes in java
+
+public class primitive_data_types {
+    public static void main(String args[])
+    {
+        //Integer
+        byte a = 127;
+        System.out.println("byte a = " + a);
+
+        short b = 129;
+        System.out.println("short b = " + b);
+
+        int c = 48273643;
+        System.out.println("int c = " + c);
+
+        long d = 1234560972823l;
+        System.out.println("long d = " + d);
+        
+        //Float
+        double e = 5.6;
+        System.out.println("double e = " + e);
+        float f = 5.6f;
+        System.out.println("float f = " + f);
+
+        //Character
+        char g = 'k';
+        System.out.println("char g = " + g);
+
+        //Boolean
+        boolean h = true;
+        System.out.println("boolean h = " + h);
+    }
+}
