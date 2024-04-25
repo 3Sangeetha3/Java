34177311cd1bfbf73a07769cbf973e8fd1e76154
sangeetha <jadamalsangeetha@gmail.com>
2024-04-25 22:34:43 +0530

    exception handling

---
+++ ExceptionHandling/MyNullPointerException.java
@@ -0,0 +1,13 @@
+package ExceptionHandling;
+
+public class MyNullPointerException {
+    public static void main(String args[]) 
+    { 
+        try { 
+            String a = null; //null value 
+            System.out.println(a.charAt(0)); 
+        } catch(NullPointerException e) { 
+            System.out.println("NullPointerException.."+ e); 
+        } 
+    }  
+}
