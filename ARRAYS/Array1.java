fcb180ff2a084003eb390c803c98d580d1b9ac43
sangeetha <jadamalsangeetha@gmail.com>
2024-05-01 22:58:39 +0530

    added: arrays

---
+++ ARRAYS/Array1.java
@@ -0,0 +1,45 @@
+package ARRAYS;
+import java.util.Scanner;
+
+public class Array1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int[] arr = new int[6];
+        String[] arr2 = new String[4];
+        double[] d = new double[5];
+
+        //taking an Integer array input from the user
+        System.out.println("Enter the integer array: ");
+        for(int i=0;i<arr.length;i++) {
+            arr[i] = in.nextInt();
+        }
+        //printing the Integer array
+        for(int i=0;i<arr.length;i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        System.out.println();
+
+        //taking a String array input from the user
+        System.out.println("\nEnter the String array: ");
+        for(int i=0;i<arr2.length;i++) {
+            arr2[i] = in.next();
+        }
+        //printing the String array
+        for(int i=0;i<arr2.length;i++) {
+            System.out.print(arr2[i] + " ");
+        }
+
+        System.out.println();
+
+        //taking a double array input from the user
+        System.out.println("\nEnter the double type array: ");
+        for(int i=0;i<d.length;i++) {
+            d[i] = in.nextDouble();
+        }
+        //print the double type array
+        for(int i=0;i<d.length;i++) {
+            System.out.print(d[i] + " ");
+        }
+    }
+}
