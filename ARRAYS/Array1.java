2b2c248b46613ed742b2b20734831279751ee3df
sangeetha <jadamalsangeetha@gmail.com>
2024-05-01 23:18:20 +0530

    added: arrays

--- ARRAYS/Array1.java
+++ ARRAYS/Array1.java
@@ -1,5 +1,6 @@
 package ARRAYS;
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Array1 {
     public static void main(String[] args) {
@@ -14,9 +15,11 @@ public class Array1 {
             arr[i] = in.nextInt();
         }
         //printing the Integer array
-        for(int i=0;i<arr.length;i++) {
-            System.out.print(arr[i] + " ");
-        }
+        // for(int i=0;i<arr.length;i++) {
+        //     System.out.print(arr[i] + " ");
+        // }
+        System.out.println(Arrays.toString(arr));
+
 
         System.out.println();
 
@@ -29,6 +32,7 @@ public class Array1 {
         for(int i=0;i<arr2.length;i++) {
             System.out.print(arr2[i] + " ");
         }
+        System.out.println(Arrays.toString(arr2));
 
         System.out.println();
 
@@ -41,5 +45,6 @@ public class Array1 {
         for(int i=0;i<d.length;i++) {
             System.out.print(d[i] + " ");
         }
+        System.out.println(Arrays.toString(d));
     }
 }
